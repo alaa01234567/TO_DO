0e634b9e355524958ce1d69fc3384bd262721736
alaa01234567 <alaaessam1011@gmail.com>
2024-08-21 20:33:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -24,42 +24,9 @@ taskInput.value = '';
 
 
 
-// let data = document.getElementsByClassName("menu")
-
-
-
-
-
-
-// let add = document.getElementsByClassName("btn");
-// let list = document.getElementsByClassName("list");
-
-// function addItem(){
-// let item =  document.createElement("li");
-// list.appendChild(item);
-// item.innerText = data.value;
-// data.value="";
-
-// }
-
-// add.addEventListener("click", addItem);
-
 
 
 
 
 
  
-// button.addEventListenter("click" , () =>{
-
-// let newElem = document.createElement("li");
-// add.appendChild(newElem);
-// newElem.innerText = data.value;
-
-// let newButton = 
-
-
-
-// })
-//  data.value=""
-// })
